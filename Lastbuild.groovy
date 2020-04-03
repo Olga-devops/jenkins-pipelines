59df84d5a87d2b8c12582032e1afce12ae57a684
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-04-02 19:34:12 -0500

    Create Lastbuild.groovy

---
+++ Lastbuild.groovy
@@ -0,0 +1,24 @@
+node { 
+properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5'))])
+  stage("Stage1"){ 
+    git 'https://github.com/farrukh90/packer.git' 
+} 
+
+  stage("Stage2"){ 
+    echo "hello" 
+} 
+
+  stage("Stage3"){ 
+    echo "hello" 
+} 
+
+  stage("Stage4"){ 
+    echo "hello" 
+} 
+
+  stage("Stage5"){ 
+    node { 
+    // some block 
+    }  
+  } 
+} 
