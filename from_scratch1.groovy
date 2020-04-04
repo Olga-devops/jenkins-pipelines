c9607b07812a3df5704a6d3d00c0d79c56ca6caa
Olga Ojjeh <Olgaojjeh.com>
2020-04-04 11:59:38 -0500

    test

---
+++ from_scratch1.groovy
@@ -0,0 +1,34 @@
+node { 
+
+properties([
+  // Discards more than 5 builds
+  buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+  // Triggers this job every minute
+  pipelineTriggers([pollSCM('* * * * *')])
+  ])
+
+
+
+
+
+
+  stage("Pull Repo"){ 
+    git "https://github.com/farrukh90/cool_website.git" 
+} 
+  stage("Install Prerequisites"){ 
+    sh """
+    sudo yum install httpd -y
+    sudo cp -r * /var/www/html/
+    sudo systemctl start httpd
+    """
+} 
+  stage("Stage3"){ 
+    echo "hello" 
+} 
+  stage("Stage4"){ 
+    echo "hello" 
+} 
+  stage("Stage5"){ 
+    echo "hello" 
+  } 
+} 
