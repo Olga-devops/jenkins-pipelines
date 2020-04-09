bfa08eb565b7c6ea01439d1b4b8d2cbae3a9c9a7
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-04-09 18:37:36 -0500

    Update artemis_delivery2.groovy

--- artemis_delivery2.groovy
+++ artemis_delivery2.groovy
@@ -9,8 +9,17 @@ properties(
 '0.4', 
 '0.5'], 
 description: 'Which version of the app should I deploy? ', 
-name: 'Version')])])
-stage("Stage1"){
+name: 'Version'),  
+      choice(choices:  
+      [ 
+        'dev1.olgaojjeh.com',  
+        'qa1.olgaojjeh.com',  
+        'stage1.olgaojjeh.com',  
+        'prod1.olgaojjeh.com'],  
+  description: 'Please provide an environment to build the application',  
+    name: 'ENVIR')])]) 
+
+  stage("Stage1"){
 timestamps {
 ws {
 checkout([$class: 'GitSCM', branches: [[name:'$Version']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/artemis.git']]])
@@ -61,4 +70,31 @@ echo "Slack"
 }
 }
 }
-}
+stage("Clean Up"){ 
+    timestamps { 
+      ws { 
+      try { 
+      sh ''' 
+        #!/bin/bash 
+        IMAGES=$(ssh centos@dev1.acirrustech.com docker ps -aq)  
+        for i in \$IMAGES; do 
+        ssh centos@dev1.acirrustech.com docker stop \$i 
+        ssh centos@dev1.acirrustech.com docker rm \$i 
+        done  
+        ''' 
+        } catch(e) { 
+        println("Script failed with error: ${e}") 
+      } 
+    } 
+  } 
+} 
+  stage("Run Container"){ 
+    timestamps { 
+    ws { 
+    sh ''' 
+    ssh centos@dev1.acirrustech.com docker run -dti -p 5001:5000 713287746880.dkr.ecr.us-east-1.amazonaws.com/artemis:${Version} 
+    ''' 
+      } 
+    } 
+  } 
+} 
